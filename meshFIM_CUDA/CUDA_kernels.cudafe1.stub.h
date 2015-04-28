42dd64eaecd75aabd96cf15016c9701f12bfe553
Brig Bagley <brig@sci.utah.edu>
2015-04-28 15:33:40 -0600

    extracted pertinent files

--- meshFIM_CUDA/CUDA_kernels.cudafe1.stub.h
+++
@@ -1,3 +0,0 @@
-extern void __device_stub__Z13run_reductionPiS_S_iS_(int *, int *, int *, int, int *);
-extern void __device_stub__Z7FIMCudaPfS_PiS0_S_S_S_S_S0_S0_S0_iiif(float *, float *, int *, int *, float *, float *, float *, float *, int *, int *, int *, int, int, int, float);
-extern void __device_stub__Z18run_check_neighborPfS_PiS0_S_S_S_S_S0_S0_S0_iiiii(float *, float *, int *, int *, float *, float *, float *, float *, int *, int *, int *, int, int, int, int, int);
--- meshFIM_CUDA/meshFIM.cudafe2.stub.c
+++
@@ -1,8 +0,0 @@
-#if defined(__cplusplus)
-extern "C" {
-#endif
-#include "meshFIM.fatbin.c"
-#include "crt/host_runtime.h"
-#if defined(__cplusplus)
-}
-#endif
